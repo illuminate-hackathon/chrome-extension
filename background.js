05ca2815a044dd79a6389c088bba2299d94149de
Brian Wyka <brian.wyka@logicmonitor.com>
2024-07-23 15:54:56 -0400

    main: Background js updates

--- background.js
+++ background.js
@@ -5,59 +5,71 @@ const HEADERS = {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
 };
-const SYSTEM_PROMPT = chrome.runtime.getURL('system-prompt.txt');
 
-async function generateSystemPrompt(pageTitle, pageURL, pageContext) {
-    const systemPromptTemplate = await fetch(SYSTEM_PROMPT)
+const generateSystemPrompt = async function (pageTitle, pageURL, pageContext) {
+    const systemPromptURL = chrome.runtime.getURL('system-prompt.txt');
+    const systemPromptTemplate = await fetch(systemPromptURL)
         .then(response => response.text());
     return systemPromptTemplate.replace('__PAGE_TITLE__', pageTitle)
         .replace('__PAGE_URL__', pageURL)
         .replace('__PAGE_CONTEXT__', pageContext);
 }
 
-async function createConversation({ userMessage: userPrompt, pageTitle, pageURL, pageContext }) {
+const requestOptions = function (method, body) {
+    return {
+        method,
+        body: JSON.stringify(body),
+        headers: HEADERS,
+    };
+}
+
+const createConversation = async function ({ userPrompt, pageTitle, pageURL, pageContext }) {
     // Prepare the request body
     const context = await generateSystemPrompt(pageTitle, pageURL, pageContext);
     const body = { context, userPrompt };
 
     // Make the API request to ilLuMinate
-    const conversationResponse = await fetch(`${BASE_API_URL}/api/conversations`, {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: HEADERS,
-    });
+    const conversationResponse = await fetch(
+        `${BASE_API_URL}/api/conversations`, 
+        requestOptions('POST', body)
+    );
+
+    // Return the response
     return await conversationResponse.json();
 }
 
-async function continueConversation(conversationId, userMessage) {
+const continueConversation = async function ({ conversationId, userMessage }) {
     // Prepare the request body
     const body = {
         userPrompt: userMessage,
     };
 
     // Make the API request to ilLuMinate
-    const conversationResponse = await fetch(`${BASE_API_URL}/api/conversations/${conversationId}`, {
-        method: 'PATCH',
-        body: JSON.stringify(body),
-        headers: HEADERS,
-    });
+    const conversationResponse = await fetch(
+        `${BASE_API_URL}/api/conversations/${conversationId}`, 
+        requestOptions('PATCH', body)
+    );
+
+    // Return the response
     return await conversationResponse.json();
 }
 
+// Listen for the extension being installed
 chrome.runtime.onInstalled.addListener(() => {
     console.log("ilLuMinate installed.");
 });
 
+// Listen for messages from the popup
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'createConversation') {
-        const { userMessage, pageContext } = request;
-        createConversation(userMessage, pageContext)
+        const { userMessage: userPrompt, pageURL, pageTitle, pageContext } = request;
+        createConversation({ userPrompt, pageURL, pageTitle, pageContext })
             .then(conversation => sendResponse(conversation));
         return true;
     }
     if (request.action === 'continueConversation') {
         const { conversationId, userMessage } = request;
-        continueConversation(conversationId, userMessage)
+        continueConversation({ conversationId, userMessage })
             .then(conversation => sendResponse(conversation));
         return true;
     }
